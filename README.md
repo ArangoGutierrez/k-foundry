9f8e253d0848eec2fd6a0ef66e2ed65ac79d6c8c
Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>
2024-04-27 08:20:39 +0200

    Init
    Signed-off-by: Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>

---
+++ README.md
@@ -0,0 +1,57 @@
+# K-Slurm
+
+## Getting Started
+
+### Running on kcp
+
+1. Run KCP with the following command:
+
+```sh
+make kcp-server
+```
+
+From this point onwards you can inspect kcp configuration using kubeconfig:
+
+```sh
+export KUBECONFIG=.test/kcp.kubeconfig
+```
+
+1. Bootstrap the KCP server with the following command:
+
+```sh
+export KUBECONFIG=./.test/kcp.kubeconfig
+make kcp-bootstrap
+```
+
+1. Run controller:
+
+```sh
+export KUBECONFIG=./.test/kcp.kubeconfig
+make run-local
+```
+
+1. In separate shell you can run tests to exercise the controller:
+
+```sh
+export KUBECONFIG=./.test/kcp.kubeconfig
+make test
+```
+
+### Uninstall resources
+To delete the resources from kcp:
+
+```sh
+make test-clean
+```
+
+
+
+### How it works
+This project aims to follow the Kubernetes [Operator pattern](https://kubernetes.io/docs/concepts/extend-kubernetes/operator/)
+
+It uses [Controllers](https://kubernetes.io/docs/concepts/architecture/controller/)
+which provides a reconcile function responsible for synchronizing resources until the desired state is reached.
+
+
+
+
---
+++ apis/v1alpha1/types.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// JobSpec defines the desired state of Job
+type JobSpec struct {
+	Script string `json:"script,omitempty"`
+}
+
+// JobStatus defines the observed state of Job
+type JobStatus struct {
+	ID int `json:"id,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Job is the Schema for the jobs API
+type Job struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   JobSpec   `json:"spec,omitempty"`
+	Status JobStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// JobList contains a list of Job
+type JobList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Job `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Job{}, &JobList{})
+}
